5744deb0be8eefd3536fe7b9035fda02087864b4
albyarto <albyarto.07@gmail.com>
2024-09-11 08:22:19 +0700

    Finising Tugas 2

--- main/models.py
+++ main/models.py
@@ -5,7 +5,3 @@ class AttributeEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-
-    # @property
-    # def is_mood_strong(self):
-    #     return self.mood_intensity > 5
