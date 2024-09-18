2d7af9d3b143bc0ca359ac59b6eff8ea687615b1
albyarto <albyarto.07@gmail.com>
2024-09-18 10:29:36 +0700

    Finishing Tugas 3

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
+import uuid
 from django.db import models
 
-# Create your models here.
-class AttributeEntry(models.Model):
+class ProductEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import ProductEntry
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
 def show_main(request):
+    product_entries = ProductEntry.objects.all()
+
     context = {
-        'name': 'Abbey Road T-Shirt',
-        'price': 'Rp 300.000',
-        'description': 'The Beatles T-Shirt',
+        'app_name' : 'Abbey Road Co.',
         'class': 'PBP D',
         'my_name': 'Muhammad Albyarto Ghazali',
+        'product_entries' : product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
